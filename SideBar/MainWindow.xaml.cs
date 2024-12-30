e974435847b4f6c7f2cf52282c4471baed119c42
unknown <jn83471@gmail.com>
2024-12-30 00:27:45 -0700

    first commit

---
+++ SideBar/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SideBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            frame.Navigate("Page1.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        private void SideMenuListControl_SelectionChanged(object sender, EventArgs e)
+        {
+            switch (MyList.SelectedIndex)
+            {
+                case 0:
+                    frame.Navigate("Page1.xaml", UriKind.RelativeOrAbsolute);
+                    break;
+                case 1:
+                    frame.Navigate("Page2.xaml", UriKind.RelativeOrAbsolute);
+                    break;
+            }
+        }
+    }
+}
